4677062985061d2227af136f0a1c24d25f901fd2
Jason Wang <jason.wang@icrar.org>
2015-02-25 00:43:00 +0800

    v0.4.18 - Minor changes to job scripts for toleration of walltime running out

--- benchmark/parallel_array_write.magnus.sh
+++ benchmark/parallel_array_write.magnus.sh
@@ -2,12 +2,12 @@
 
 for i in $(seq 1 1 9)
 do
-	sbatch --account=$PROJECT --time=12:00:00 --nodes=$i --ntasks-per-node=24 --dependency=singleton parallel_array_write.sh
+	sbatch --account=$PROJECT --time=12:00:00 --nodes=$i --ntasks-per-node=1 --dependency=singleton parallel_array_write.sh
 done
 
 for i in $(seq 10 10 100)
 do
-	sbatch --account=$PROJECT --time=12:00:00 --nodes=$i --ntasks-per-node=24 --dependency=singleton parallel_array_write.sh
+	sbatch --account=$PROJECT --time=12:00:00 --nodes=$i --ntasks-per-node=1 --dependency=singleton parallel_array_write.sh
 done
 
 
--- benchmark/parallel_array_write.sh
+++ benchmark/parallel_array_write.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 QUOTA="5000000000" # in KB
-prefix=$scratch/stripe8
+prefix=$scratch/stripe4
 
-for rows in $(seq 100 100 1000) 
+for i in $(seq 1 10)
 do
-	for length in $(seq 1000 1000 20000)
+	for rows in $(seq 100 100 1000) 
 	do
-
-		for i in $(seq 1 10)
+		for length in $(seq 1000 1000 20000)
 		do
+
 
 			echo $rows $length $length $prefix/${rows}rows_${length}length_${SLURM_JOBID}_${i}.casa
 
@@ -27,6 +27,7 @@ do
 			if [ "$JOBSCHEDULER" == "pbs" ]; then
 				mpirun $PBS_O_WORKDIR/parallel_array_write $rows $length $length $prefix/${rows}rows_${length}length_${PBS_JOBID}_${i}.casa  >> $PBS_O_WORKDIR/log
 			fi
+		
 
 		done
 
